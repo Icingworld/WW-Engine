a309a8d26efab4b1a7d66aec30cd22a26f79b3f9
icingworld <984442022@qq.com>
2025-01-24 11:13:01 +0800

    fix: remove font from ImGuiLayer onAttach

--- functions/imgui/ww_imgui_layer.cpp
+++ functions/imgui/ww_imgui_layer.cpp
@@ -31,11 +31,7 @@ void ImGuiLayer::onAttach()
     IMGUI_CHECKVERSION();           // 检查 ImGui 版本
     ImGui::CreateContext();         // 创建 ImGui 上下文
     ImGuiIO & io = ImGui::GetIO();  // 获取 ImGui IO
-     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // 启用键盘导航
-
-    float fontSize = 18.0f;         // 设置字体大小*2.0f;
-    io.Fonts->AddFontFromFileTTF("C:/Users/zwjiang/Desktop/Test/WWCraft/engine/functions/gui/assets/fonts/OpenSans-Regular.ttf", fontSize);
-    io.FontDefault = io.Fonts->AddFontFromFileTTF("C:/Users/zwjiang/Desktop/Test/WWCraft/engine/functions/gui/assets/fonts/OpenSans-Regular.ttf", fontSize);
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // 启用键盘导航
 
     ImGui::StyleColorsDark();
 
