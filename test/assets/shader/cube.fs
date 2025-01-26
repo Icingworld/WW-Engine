7ee938301ead31e4943910087e80b4e96ec7f2f7
icingworld <984442022@qq.com>
2025-01-26 10:47:23 +0800

    feat: update glsl for example cube with texture

--- test/assets/shader/cube.fs
+++ test/assets/shader/cube.fs
@@ -1,8 +1,10 @@
 #version 330 core
 in vec3 Color;
+in vec2 TexCoords;
 out vec4 FragColor;
+uniform sampler2D Texture0;
 
 void main()
 {
-    FragColor = vec4(Color, 1.0);
+    FragColor = texture(Texture0, TexCoords);
 }
