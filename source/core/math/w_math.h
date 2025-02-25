af52c1888f37b294f4800543f3207a9c2d04c5e0
icingworld <984442022@qq.com>
2025-02-25 09:31:27 +0800

    feat: create math to core
    - Currently, I choose to use GLM, so just using it's namespace

---
+++ source/core/math/w_math.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace engine
+{
+
+using namespace glm;
+
+} // namespace engine
