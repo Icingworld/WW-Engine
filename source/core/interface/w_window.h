9dcc077bd9cf90df2630bfc9602db0ef683662c3
Icingworld <984442022@qq.com>
2025-02-27 00:55:26 +0800

    feat: create window base

---
+++ source/core/interface/w_window.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+#include "w_event.h"
+
+namespace engine
+{
+
+/**
+ * @brief 窗口
+ */
+class WWindow
+{
+
+};
+
+} // namespace engine
