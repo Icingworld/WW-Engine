1094c5a8639aa29ae8cf726b1f2e497997c5c25b
icingworld <984442022@qq.com>
2025-02-26 15:56:33 +0800

    feat: create keycode and mousecode

---
+++ source/core/global/w_mouse_code.h
@@ -0,0 +1,33 @@
+#pragma once
+
+namespace engine
+{
+
+/**
+ * @brief 鼠标码储存类型
+ */
+using mouse_code_type = unsigned char;
+
+/**
+ * @brief 鼠标码
+ */
+enum class MouseCode
+    : mouse_code_type
+{
+    // From glfw3.h
+    Button0                = 0,
+    Button1                = 1,
+    Button2                = 2,
+    Button3                = 3,
+    Button4                = 4,
+    Button5                = 5,
+    Button6                = 6,
+    Button7                = 7,
+
+    ButtonLast             = Button7,
+    ButtonLeft             = Button0,
+    ButtonRight            = Button1,
+    ButtonMiddle           = Button2
+};
+
+} // namespace engine
