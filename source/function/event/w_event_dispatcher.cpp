96c9ee7144155afc87e99684c0b6ada3e28a9636
icingworld <984442022@qq.com>
2025-02-27 14:28:13 +0800

    feat: create event dispatcher

---
+++ source/function/event/w_event_dispatcher.cpp
@@ -0,0 +1,11 @@
+#include "w_event_dispatcher.h"
+
+namespace engine
+{
+
+WEventDispatcher::WEventDispatcher(const WEvent & event)
+    : m_event(event)
+{
+}
+
+} // namespace engine
---
+++ source/function/event/w_event_dispatcher.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "w_event.h"
+
+namespace engine
+{
+
+/**
+ * @brief 事件分发器
+ */
+class WEventDispatcher
+{
+protected:
+    const WEvent & m_event;        // 事件对象
+
+public:
+    explicit WEventDispatcher(const WEvent & event);
+
+    ~WEventDispatcher() = default;
+
+public:
+    /**
+     * @brief 分发事件
+     * @details 分发事件到事件回调函数
+     */
+    template <
+        class EventType,
+        class Function
+    > bool dispatch(const Function & func)
+    {
+        if (m_event.getEventType() == EventType::getEventType()) {
+            // 将事件强制转换为具体类型并调用回调函数
+            m_event.setHandled(func(static_cast<EventType &>(m_event)));
+            return true;
+        }
+        return false;
+    }
+};
+
+} // namespace engine
