09e985e0671247eb72a7335c4be592bbb6f1e026
icingworld <984442022@qq.com>
2025-02-27 13:55:52 +0800

    feat: create layerstack

---
+++ source/function/layer/w_layer_stack.cpp
@@ -0,0 +1,108 @@
+#include "w_layer_stack.h"
+
+#include <algorithm>
+
+namespace engine
+{
+
+WLayerStack::~WLayerStack()
+{
+    for (WLayer * layer : m_layers) {
+        layer->onDetach();
+        delete layer;
+    }
+}
+
+WLayerStack::iterator WLayerStack::begin()
+{
+    return m_layers.begin();
+}
+
+WLayerStack::const_iterator WLayerStack::begin() const
+{
+    return m_layers.begin();
+}
+
+WLayerStack::const_iterator WLayerStack::cbegin() const
+{
+    return m_layers.cbegin();
+}
+
+WLayerStack::iterator WLayerStack::end()
+{
+    return m_layers.end();
+}
+
+WLayerStack::const_iterator WLayerStack::end() const
+{
+    return m_layers.end();
+}
+
+WLayerStack::const_iterator WLayerStack::cend() const
+{
+    return m_layers.cend();
+}
+
+WLayerStack::reverse_iterator WLayerStack::rbegin()
+{
+    return m_layers.rbegin();
+}
+
+WLayerStack::const_reverse_iterator WLayerStack::rbegin() const
+{
+    return m_layers.rbegin();
+}
+
+WLayerStack::const_reverse_iterator WLayerStack::crbegin() const
+{
+    return m_layers.crbegin();
+}
+
+WLayerStack::reverse_iterator WLayerStack::rend()
+{
+    return m_layers.rend();
+}
+
+WLayerStack::const_reverse_iterator WLayerStack::rend() const
+{
+    return m_layers.rend();
+}
+
+WLayerStack::const_reverse_iterator WLayerStack::crend() const
+{
+    return m_layers.crend();
+}
+
+void WLayerStack::pushLayer(WLayer * layer)
+{
+    m_layers.emplace(m_layers.begin() + m_layerInsertIndex, layer);
+    layer->onAttach();
+    ++m_layerInsertIndex;
+}
+
+void WLayerStack::pushOverlay(WLayer * layer)
+{
+    m_layers.emplace_back(layer);
+    layer->onAttach();
+}
+
+void WLayerStack::popLayer(WLayer * layer)
+{
+    auto it = std::find(m_layers.begin(), m_layers.begin() + m_layerInsertIndex, layer);
+    if (it != m_layers.begin() + m_layerInsertIndex) {
+        layer->onDetach();
+        m_layers.erase(it);
+        --m_layerInsertIndex;
+    }
+}
+
+void WLayerStack::popOverlay(WLayer * layer)
+{
+    auto it = std::find(m_layers.begin() + m_layerInsertIndex, m_layers.end(), layer);
+    if (it != m_layers.end()) {
+        layer->onDetach();
+        m_layers.erase(it);
+    }
+}
+
+} // namespace engine
---
+++ source/function/layer/w_layer_stack.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <vector>
+
+#include "w_layer.h"
+
+namespace engine
+{
+
+/**
+ * @brief 图层栈
+ */
+class WLayerStack
+{
+public:
+    using iterator = std::vector<WLayer *>::iterator;
+    using const_iterator = std::vector<WLayer *>::const_iterator;
+    using reverse_iterator = std::vector<WLayer *>::reverse_iterator;
+    using const_reverse_iterator = std::vector<WLayer *>::const_reverse_iterator;
+
+protected:
+    std::vector<WLayer *> m_layers;         // 图层栈
+    std::size_t m_layerInsertIndex;         // 图层插入索引
+
+public:
+    WLayerStack() = default;
+
+    ~WLayerStack();
+
+public:
+    // 迭代器
+
+    iterator begin();
+
+    const_iterator begin() const;
+
+    const_iterator cbegin() const;
+
+    iterator end();
+
+    const_iterator end() const;
+
+    const_iterator cend() const;
+
+    reverse_iterator rbegin();
+
+    const_reverse_iterator rbegin() const;
+
+    const_reverse_iterator crbegin() const;
+
+    reverse_iterator rend();
+
+    const_reverse_iterator rend() const;
+
+    const_reverse_iterator crend() const;
+
+public:
+    /**
+     * @brief 添加普通图层
+     * @details 通常是底层图层，先被渲染
+     */
+    void pushLayer(WLayer * WLayer);
+
+    /**
+     * @brief 添加覆盖图层
+     * @details 通常是顶层图层，后被渲染，起覆盖作用
+     */
+    void pushOverlay(WLayer * WLayer);
+
+    /**
+     * @brief 移除普通图层
+     */
+    void popLayer(WLayer * WLayer);
+
+    /**
+     * @brief 移除覆盖图层
+     */
+    void popOverlay(WLayer * WLayer);
+};
+
+} // namespace engine
